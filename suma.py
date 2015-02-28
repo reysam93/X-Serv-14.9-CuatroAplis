dd6491bce7e340368c9afa58211c8bf618eec440
Samuel <samuel@samuel.(none)>
2015-02-28 19:06:37 +0100

    ejecutar desde hola.py

--- suma.py
+++ suma.py
@@ -5,10 +5,10 @@ import webappmulti
 
 
 class mySumApp(webappmulti.app):
-    
+
     def __init__(self):
         self.first = None
-    
+
     def parse(self, request, rest):
         try:
             print "rest: " + rest
@@ -16,10 +16,10 @@ class mySumApp(webappmulti.app):
         except ValueError:
             return None
         return num
-        
+
     def process(self, request):
         if not request:
-            return("404 Bad Request", "Only numbers") 
+            return("404 Bad Request", "Only numbers")
         if self.first is None:
             self.first = request
             respond = "Primer numero = " + str(request)
@@ -30,5 +30,5 @@ class mySumApp(webappmulti.app):
         return ("200 OK", "<html><body><p>" + respond + "</p></body></html>")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     sumador = mySumApp("localhost", 9999)
